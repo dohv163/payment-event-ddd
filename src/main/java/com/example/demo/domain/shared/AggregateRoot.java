ff8a031e466d6e7d35861c972829293dcead79e3
dohv <dohv@ftech.ai>
2022-12-28 14:14:53 +0700

    update code

--- src/main/java/com/example/demo/domain/shared/AggregateRoot.java
+++ src/main/java/com/example/demo/domain/shared/AggregateRoot.java
@@ -1,13 +1,50 @@
 package com.example.demo.domain.shared;
 
 import java.io.Serializable;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.context.ApplicationContext;
 
 public abstract class AggregateRoot <E, ID extends Serializable> implements Entity<E, ID>{
 
+  public final ID entityId;
+  private final ApplicationContext applicationContext;
+  private AggregateRootBehavior behavior;
+
+  protected AggregateRoot(ApplicationContext applicationContext, ID entityId) {
+    this.entityId = entityId;
+    this.applicationContext = applicationContext;
+    this.behavior = initialBehavior();
+  }
 
   protected abstract AggregateRootBehavior initialBehavior();
 
   public class AggregateRootBehavior<ID> {
+
+    protected final Map<Class<? extends Command>, CommandHandler<? extends Command, ? extends Event, ID>> handlers;
+
+    public AggregateRootBehavior(Map<Class<? extends Command>, CommandHandler<? extends Command, ? extends Event, ID>> handlers) {
+      this.handlers = Collections.unmodifiableMap(handlers);
+    }
+
+    protected <A extends Command, B extends Event> CommandHandler<A, B, ID> getHandler(Class<? extends CommandHandler> commandHandlerClass) {
+      return applicationContext.getBean(commandHandlerClass);
+    }
+
+    public class AggregateRootBehaviorBuilder<ID> {
+
+      private final Map<Class<? extends Command>, CommandHandler<? extends Command, ? extends Event, ID>> handlers = new HashMap<>();
+
+      public <A extends Command, B extends Event> AggregateRootBehaviorBuilder setCommandHandler(Class<A> commandClass, CommandHandler<A, B, ID> handler) {
+        handlers.put(commandClass, handler);
+        return this;
+      }
+
+      public AggregateRootBehavior build() {
+        return new AggregateRootBehavior(handlers);
+      }
+    }
 
   }
 
--- src/main/java/com/example/demo/domain/shared/CommandHandler.java
+++ src/main/java/com/example/demo/domain/shared/CommandHandler.java
@@ -1,5 +1,9 @@
 package com.example.demo.domain.shared;
 
+import io.vavr.control.Either;
+import java.util.concurrent.CompletionStage;
+
 public interface CommandHandler<C extends Command, E extends Event, ID> {
+  CompletionStage<Either<CommandFailure, E>> handle(C command, ID entityId);
 
 }
